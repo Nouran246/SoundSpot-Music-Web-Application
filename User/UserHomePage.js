5d3dc3648c7e73637b6db97da1920fe0cfeee82c
Laila Amgad ElZawawy <125660987+Laila4563@users.noreply.github.com>
2024-04-12 21:34:02 +0200

    Update UserHomePage.js

--- User/UserHomePage.js
+++ User/UserHomePage.js
@@ -1,9 +1,10 @@
 function toggleNavbar() {
     var navbar = document.querySelector('.navigationbar');
+    var logo = document.getElementById('logo');
     navbar.classList.toggle('minimized');
-    var trialImg = document.querySelector('#trial-img');
-    trialImg.classList.toggle('minimized');
+    logo.style.display = navbar.classList.contains('minimized') ? 'block' : 'none';
 }
+
 document.addEventListener("DOMContentLoaded", function () {
     var profileIcon = document.getElementById("user");
     var dropdownMenu = document.querySelector(".dropdown-content");
@@ -20,5 +21,27 @@ document.addEventListener("DOMContentLoaded", function () {
     profileIcon.addEventListener("click", function () {
         
         dropdownMenu.classList.toggle(".show");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var toggleLightModeButton = document.getElementById("toggle-light-mode");
+    var body = document.querySelector("body");
+    var navigationBar = document.querySelector(".navigationbar");
+    var header = document.querySelector(".header");
+    var foot=document.querySelector("footer");
+    var too = document.querySelector(".toggle-button");
+    var dc = document.querySelector(".dropdown-content");
+    var lightbutton = document.querySelector("#toggle-light-mode");
+    
+    toggleLightModeButton.addEventListener("click", function() {
+        body.classList.toggle("light-mode");
+        navigationBar.classList.toggle("light-mode");
+        header.classList.toggle("light-mode");
+        too.classList.toggle("light-mode");
+        foot.classList.toggle("light-mode");
+        dc.classList.toggle("light-mode");
+        lightbutton.classList.toggle("light-mode");
+        
     });
 });
