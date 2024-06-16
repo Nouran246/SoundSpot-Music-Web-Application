0a076531181bc247ef341ec0900edf5d82b8ac47
YahiaSwag <yahiawyyoyo@gmail.com>
2024-06-16 22:35:16 +0300

    minor session logout fix

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -5,13 +5,22 @@ const authMiddleware = require("../controllers/authMiddleware");
 app.get("/", (req, res) => {
   res.render("landing", {
     currentPage: "landing",
-    user: req.session.user === undefined ? "" : req.session.user,
+    user: req.session.user || "",
   });
 });
-app.get("/index",(req, res) => {
-  res.render("index", {
-    currentPage: "index",
-    user: req.session.user === undefined ? "" : req.session.user,
-  });
+
+// Index page (login/signup form)
+app.get("/index", (req, res) => {
+  // Check if user is logged in
+  if (req.session.user) {
+    // Redirect to another page (e.g., dashboard) if logged in
+    res.redirect("/auth/UserHomePage"); // Replace with your desired redirect URL
+  } else {
+    // Render the index page if not logged in
+    res.render("index", {
+      currentPage: "index",
+      user: req.session.user || "",
+    });
+  }
 });
 module.exports = app;
