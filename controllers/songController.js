e50272a18d353cba9091dda66812ddf5ed4e0737
YahiaSwag <yahiawyyoyo@gmail.com>
2024-06-17 18:27:23 +0300

    song writes in db

---
+++ controllers/songController.js
@@ -0,0 +1,30 @@
+// songController.js
+
+const Song = require('../models/song');
+
+exports.uploadSong = async (req, res) => {
+  const { songName, artistName, genre, albumName, releaseDate, keywords, lyrics } = req.body;
+  console.log("Received data:", req.body);
+  const songFile = req.files.songFileId[0];
+  const imageFile = req.files.imageFileId[0];
+
+  try {
+    const newSong = new Song({
+      songName: songName,
+      artistName: artistName,
+      genre: genre,
+      albumName: albumName,
+      releaseDate: releaseDate,
+      keywords: keywords,
+      lyrics: lyrics,
+      songFileId: songFile.filename,
+      imageFileId: imageFile.filename
+    });
+
+    const savedSong = await newSong.save();
+    res.status(200).json(savedSong);
+  } catch (error) {
+    console.error('Error uploading song:', error);
+    res.status(500).send('Internal Server Error');
+  }
+};
--- routes/routes.js
+++ routes/routes.js
@@ -23,7 +23,9 @@ const storage = multer.diskStorage({
     cb(null, uniqueSuffix + path.extname(file.originalname)); // Add original extension
   }
 });
-const upload = multer({ dest: 'uploads/' }); // Ensure the 'uploads' directory exists
+
+const upload = multer({ storage: storage });
+
 const router = express.Router();
 
 function setupRoutes(app) {
@@ -34,6 +36,14 @@ function setupRoutes(app) {
   app.use("/auth", authRoutes);
   app.use("/user", userRoutes);
 
+<<<<<<< Updated upstream
+=======
+  // Adjust the field names to match those in the form
+  app.post('/songs/upload', upload.fields([
+    { name: 'songFileId', maxCount: 1 },
+    { name: 'imageFileId', maxCount: 1 }
+  ]), songController.uploadSong);
+>>>>>>> Stashed changes
 
   // Route for processing plan creation with file uploads
   app.post('/plans/process', upload.fields([{ name: 'adsVideo', maxCount: 1 }, { name: 'popupImage', maxCount: 1 }]), planController.createPlan);
